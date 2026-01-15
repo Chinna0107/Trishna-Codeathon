afe018ea5c535bcbbe08fe878f79cf46b6eda80d
chinna0107 <kancharlahemanth89@gmail.com>
2026-01-15 16:30:23 +0530

    Prod 1.1

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const config = {
   LOCAL_BASE_URL: 'http://localhost:5000',
-  BASE_URL: 'https://your-production-url.com',
+  BASE_URL: 'https://tri-cod-be.vercel.app',
   API_BASE_URL: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:5000/api' 
-    : 'https://your-production-url.com/api'
+    : 'https://tri-cod-be.vercel.app/api'
 };
 
 export default config;
